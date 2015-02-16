435dfd54b7fc992cd7bdf29218f44e46999b8b73
tkingkwun <kingkwun.tsang@nxtomo.hk>
2015-02-16 16:42:59 +0800

    update tutorial 5

--- Qt_tutorial_framework/BasicTutorial5.cpp
+++ Qt_tutorial_framework/BasicTutorial5.cpp
@@ -18,10 +18,157 @@ BasicTutorial5::~BasicTutorial5(void)
 void BasicTutorial5::createScene(void)
 {
     // create your scene here :)
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.25, 0.25, 0.25));
 
+            // add the ninja
+        Ogre::Entity *ent = mSceneMgr->createEntity("Ninja", "ninja.mesh");
+            Ogre::SceneNode *node = mSceneMgr->getRootSceneNode()->createChildSceneNode("NinjaNode");
+            node->attachObject(ent);
+
+            // create the light
+            Ogre::Light *light = mSceneMgr->createLight("Light1");
+            light->setType(Ogre::Light::LT_POINT);
+            light->setPosition(Ogre::Vector3(250, 150, 250));
+            light->setDiffuseColour(Ogre::ColourValue::White);
+            light->setSpecularColour(Ogre::ColourValue::White);
+
+           // Create the scene node
+            node = mSceneMgr->getRootSceneNode()->createChildSceneNode("CamNode1", Ogre::Vector3(-400, 200, 400));
+
+            // Make it look towards the ninja
+            node->yaw(Ogre::Degree(-45));
+
+            // Create the pitch node
+            node = node->createChildSceneNode("PitchNode1");
+            node->attachObject(mCamera);
+
+            // create the second camera node/pitch node
+            node = mSceneMgr->getRootSceneNode()->createChildSceneNode("CamNode2", Ogre::Vector3(0, 200, 400));
+            node = node->createChildSceneNode("PitchNode2");
 }
 
+void BasicTutorial5::createFrameListener(void)
+{
+    BaseApplication::createFrameListener();
 
+    // set the rotation and move speed
+    mRotate = 0.13;
+    mMove = 250;
+
+    mDirection = Ogre::Vector3::ZERO;
+}
+
+bool BasicTutorial5::frameRenderingQueued(const Ogre::FrameEvent& evt)
+{
+    if (mWindow->isClosed()) return false;
+        if (mShutDown) return false;
+        mKeyboard->capture();
+        mMouse->capture();
+        mTrayMgr->frameRenderingQueued(evt);
+
+        // Populate the camera container
+        mCamNode = mCamera->getParentSceneNode();
+
+        mCamNode->translate(mDirection * evt.timeSinceLastFrame, Ogre::Node::TS_LOCAL);
+
+        return true;
+}
+
+// OIS::KeyListener
+bool BasicTutorial5::keyPressed( const OIS::KeyEvent& evt ){
+    switch (evt.key)
+    {
+    case OIS::KC_ESCAPE:
+        mShutDown = true;
+        break;
+    case OIS::KC_1:
+        mCamera->getParentSceneNode()->detachObject(mCamera);
+        mCamNode = mSceneMgr->getSceneNode("CamNode1");
+        mCamNode->attachObject(mCamera);
+        break;
+
+    case OIS::KC_2:
+        mCamera->getParentSceneNode()->detachObject(mCamera);
+        mCamNode = mSceneMgr->getSceneNode("CamNode2");
+        mCamNode->attachObject(mCamera);
+        break;
+    case OIS::KC_UP:
+    case OIS::KC_W:
+        mDirection.z = -mMove;
+        std::cout<<"mDirection.z : " << mDirection.z << std::endl;
+        break;
+
+    case OIS::KC_DOWN:
+    case OIS::KC_S:
+        mDirection.z = mMove;
+        break;
+
+    case OIS::KC_LEFT:
+    case OIS::KC_A:
+        mDirection.x = -mMove;
+        break;
+
+    case OIS::KC_RIGHT:
+    case OIS::KC_D:
+        mDirection.x = mMove;
+        break;
+
+    case OIS::KC_PGDOWN:
+    case OIS::KC_E:
+        mDirection.y = -mMove;
+        break;
+
+    case OIS::KC_PGUP:
+    case OIS::KC_Q:
+        mDirection.y = mMove;
+        break;
+    default:
+        break;
+    }
+    return true;
+}
+bool BasicTutorial5::keyReleased( const OIS::KeyEvent& evt ){
+    switch (evt.key)
+    {
+    case OIS::KC_UP:
+    case OIS::KC_W:
+        mDirection.z = 0;
+        break;
+
+    case OIS::KC_DOWN:
+    case OIS::KC_S:
+        mDirection.z = 0;
+        break;
+
+    case OIS::KC_LEFT:
+    case OIS::KC_A:
+        mDirection.x = 0;
+        break;
+
+    case OIS::KC_RIGHT:
+    case OIS::KC_D:
+        mDirection.x = 0;
+        break;
+
+    case OIS::KC_PGDOWN:
+    case OIS::KC_E:
+        mDirection.y = 0;
+        break;
+
+    case OIS::KC_PGUP:
+    case OIS::KC_Q:
+        mDirection.y = 0;
+        break;
+
+    default:
+        break;
+    }
+    return true;
+}
+// OIS::MouseListener
+bool BasicTutorial5::mouseMoved( const OIS::MouseEvent& evt ){return true;}
+bool BasicTutorial5::mousePressed( const OIS::MouseEvent& evt, OIS::MouseButtonID id ){return true;}
+bool BasicTutorial5::mouseReleased( const OIS::MouseEvent& evt, OIS::MouseButtonID id ){return true;}
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
--- Qt_tutorial_framework/BasicTutorial5.h
+++ Qt_tutorial_framework/BasicTutorial5.h
@@ -16,8 +16,22 @@ public:
 
 protected:
     virtual void createScene(void);
-    virtual void createCamera(void);
-    virtual void createViewports(void);
+    virtual void createFrameListener(void);
+
+    // Ogre::FrameListener
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt );
+    // OIS::KeyListener
+    virtual bool keyPressed( const OIS::KeyEvent& evt );
+    virtual bool keyReleased( const OIS::KeyEvent& evt );
+    // OIS::MouseListener
+    virtual bool mouseMoved( const OIS::MouseEvent& evt );
+    virtual bool mousePressed( const OIS::MouseEvent& evt, OIS::MouseButtonID id );
+    virtual bool mouseReleased( const OIS::MouseEvent& evt, OIS::MouseButtonID id );
+
+    Ogre::Real mRotate;          // The rotate constant
+    Ogre::Real mMove;            // The movement constant
+    Ogre::SceneNode *mCamNode;   // The SceneNode the camera is currently attached to
+    Ogre::Vector3 mDirection;    // Value to move in the correct direction
 };
 
 #endif // #ifndef __BasicTutorial5_h_
